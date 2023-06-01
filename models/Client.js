1a9933a9f0c9138dc4c77a5c3da625cba851adcb
Sami Khan <samikhan.dev@gmail.com>
2023-06-01 17:51:25 -0400

    Created Client and Project Model in our applicaiton

---
+++ models/Client.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const ClientSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: 'Invalid email address format',
+    },
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model("Client", ClientSchema);
---
+++ models/Project.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Not Started", "In Progress", "Completed"],
+  },
+  clientId: {
+    type: Schema.Types.ObjectId,
+    ref: "Client",
+  },
+});
+
+export default model("Project", ProjectSchema);
